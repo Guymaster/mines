abf15c486499b61eddc3a654acb513162e4788f7
Guymaster <guymaster25@gmail.com>
2023-12-26 17:03:03 +0000

    Setting up cell behavior

--- src/app/game/[game_id]/cell.component.tsx
+++ src/app/game/[game_id]/cell.component.tsx
@@ -1,19 +1,35 @@
 'use client'
 
 import { CellModel } from '@/models/cell.model';
+import { useGameRoomContext } from '@/providers/game_room.provider';
+import { ClientMessagesTypes } from '@/values/game';
 import { Card } from '@chakra-ui/react'
 
 export default function Cell(props: {size: number, data: CellModel}) {
+  const {gameRoom, setGameRoom} = useGameRoomContext();
 
   const handleClick = (event: any) => {
     console.log(`Clicked on cell ${props.data.row}:${props.data.col}`);
+    if(!gameRoom){
+      return;
+    }
+    gameRoom.send(ClientMessagesTypes.CURSOR_POSITION, {
+      col: props.data.col,
+      row: props.data.row
+    })
   };
-
   return (
     <Card width={props.size} height={props.size} backgroundColor={"gray"} _hover={{backgroundColor: "green"}} borderRadius={0} onClick={handleClick}>
-      {props.data.content &&
-        (props.data.content.isBomb)?
-          "B" : `${props.data.content?.number}`
+      {
+        (() => {
+          if(!props.data.content){
+            return "";
+          }
+          if(props.data.content.isBomb){
+            return "B";
+          }
+          return props.data.content.number.toString();
+        })()
       }
     </Card>
   );
